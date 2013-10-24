1b81241034f859fd528c3b2f1b87c96446737a82
Kata <lightquake@amateurtopologist.com>
2013-10-24 16:37:15 -0400

    Move font into local.el.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -151,7 +151,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#eaeaea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Inconsolata"))))
  '(diff-added ((t (:foreground "green3"))))
  '(diff-file-header ((t (:weight bold))))
  '(diff-header ((t nil)))
