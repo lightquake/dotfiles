d056f10b9a792a658822c8ca48a848eeb0de2131
Kata <lightquake@amateurtopologist.com>
2013-12-06 18:57:49 -0500

    Enable paredit when editing lisps.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,6 +44,10 @@
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
+;; Editing lisps without paredit is incredibly painful
+(add-hook 'lisp-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
 ;; Get the right $PATH.
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
